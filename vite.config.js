0f38e4533c530c20d751d4befe4a02fd74267ba2
nkorolev <orevdrol@mail.ru>
2024-10-17 12:23:46 +0300

    project initial state - add vite config - add base folder structure - add assets

---
+++ vite.config.js
@@ -0,0 +1,61 @@
+import vituum from 'vituum'
+import { fileURLToPath, URL } from 'node:url'
+import pug from '@vituum/vite-plugin-pug'
+import pages from 'vituum/plugins/pages.js'
+import imports from "vituum/plugins/imports.js";
+
+export default {
+    resolve: {
+        alias: {
+            '@': fileURLToPath(new URL('./src', import.meta.url)),
+        }
+    },
+    base: './',
+    plugins: [
+        vituum(),
+        pug({root: '/src',}),
+        imports({
+            filenamePattern: {
+                'src/styles': '+.scss'
+            },
+            paths: ['/src/styles/*/**', '/src/scripts/*/**', '/src/assets/*/**'],
+        }),
+        pages({
+            dir: './src/templates/pages',
+            root: './src',
+            normalizeBasePath: true
+        },),
+    ],
+    assets: {
+        fileExtensions: ['jpg', 'jpeg', 'png', 'svg', 'ico', 'woff', 'woff2', 'ttf'],
+    },
+    build: {
+        minify: true,
+        rollupOptions: {
+            input: [
+                './src/templates/pages/*.{pug,html}',
+                './src/styles/*.{css,scss}',
+                './src/scripts/**/*.{js}',
+                './src/assets/**/*.{svg,png,jpeg,jpg,woof,woof2,ttf}',
+            ],
+            output: {
+                chunkFileNames: 'scripts/[name].js',
+                entryFileNames: 'scripts/[name].js',
+                assetFileNames: ({name}) => {
+                    if (/\.css$/.test(name ?? '')) {
+                        return '[name][extname]';
+                    }
+                    if (/\.(webp|png|jpg|jpeg|gif|ico|svg)$/.test(name ?? '')) {
+                        return 'assets/img/[name][extname]';
+                    }
+                    if (/\.(woff|woff2|ttf)$/.test(name ?? '')) {
+                        return 'assets/fonts/[name][extname]';
+                    }
+                    return 'assets/[name][extname]'
+                },
+            }
+
+        }
+    },
+
+}
